5a71c8913d309514eb34ef7d3e72be904eee4ff6
Marco <marco.spaziani.brunella@unirma2.it>
2021-12-14 15:45:22 +0100

    Fixed divider; Added Verilator testbench specifically for divider

--- design/exu/obj_dir/Vexu_div_ctl_32__ALLcls.d
+++
@@ -1,5 +0,0 @@
-Vexu_div_ctl_32__ALLcls.o: Vexu_div_ctl_32__ALLcls.cpp \
- Vexu_div_ctl_32.cpp Vexu_div_ctl_32.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vexu_div_ctl_32__Syms.h \
- Vexu_div_ctl_32___024unit.h Vexu_div_ctl_32___024unit.cpp
--- design/exu/obj_dir/tb_div32.d
+++
@@ -1,5 +0,0 @@
-tb_div32.o: ../tb_div32.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h Vexu_div_ctl_32.h \
- Vexu_div_ctl_32___024unit.h
