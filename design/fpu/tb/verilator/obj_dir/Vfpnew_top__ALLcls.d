289dba87255e69f1fe29d6913c6edcfc73f183d9
Marco Spaziani Brunella <marco.spaziani.brunella@uniroma2.it>
2022-01-25 11:37:33 +0100

    Cleaned-up directory structure for FPU

--- design/fpu/tb/verilator/obj_dir/Vfpnew_top__ALLcls.d
+++
@@ -1,4 +0,0 @@
-Vfpnew_top__ALLcls.o: Vfpnew_top__ALLcls.cpp Vfpnew_top.cpp Vfpnew_top.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vfpnew_top__Syms.h \
- Vfpnew_top___024unit.h Vfpnew_top___024unit.cpp
